ebe83c935e598e7c744174dd53f98e610018a5d9
kapil jain <kapil.kapil231991@hotmail.com>
2018-01-12 00:16:48 +0530

    second push

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,6 +4,9 @@ public class TestClass {
 		System.out.println("hello from dev");
 		
 	}
+	publi String test(){
+		return "hello";
+	}
 	public static void main(String args[]){
 		System.out.println("hello");
 		TestClass t1 = new TestClass();
